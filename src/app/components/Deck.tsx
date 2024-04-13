99e4a0cf6ce4691e08ba2a33cd2511d635e84c7f
Keith <keithzano@gmail.com>
2024-04-13 10:27:49 +0200

    Add shuffled deck of cards

--- src/app/components/Deck.tsx
+++ src/app/components/Deck.tsx
@@ -1,4 +1,43 @@
+import { useEffect, useState } from "react"
+import { Card } from "../utils/Types";
+
 export const Deck:React.FC = ()=>{
+    const [cards, setCards] = useState<Card[]>([])
+    
+    useEffect(()=>{
+        const suits:string[] = ['hearts', 'diamonds', 'clubs', 'spades'];
+        const ranks:string[] = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
+        const jokers:Card[] = [{id: 53,rank: 'joker', suit: 'black', color: 'red'},{id: 54,rank: 'joker', suit: 'red', color: 'red'}];
+
+        let deck:Card[] = [];
+        let id = 1;
+        for (const suit of suits){
+            let color:'red' | 'black';
+            if (suit === 'hearts' || suit === 'diamonds'){
+                color = 'red';
+            }else{
+                color = 'black'
+            }
+            for (const rank of ranks){
+                deck.push({id, suit, rank, color})
+                id++
+            }
+        }
+        deck.push(...jokers);
+        // Shuffling the cards
+        const shuffle = (deck:Card[]):Card[]=>{
+            
+            for (let i = deck.length - 1; i > 0; i--) {
+                const j = Math.floor(Math.random() * (i + 1));
+                [deck[i], deck[j]] = [deck[j], deck[i]];
+              }
+              return deck;
+        }
+        setCards(shuffle(deck));
+    },[])
+
+    console.log(cards)
+
     return (
         <></>
     )
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+'use client'
+import { Deck } from "./components/Deck";
+
 export default function Home() {
   return (
     <main className=" max-w-[1440px] mx-auto max-h-[1024] h-screen">
-
+      <Deck/>
     </main>
   );
 }
